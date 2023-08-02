31372dbbf46a21fe07a06abe972e1c62c2e65788
lisovenkoNastya <lisovenko.ad@gmail.com>
2023-08-02 18:30:20 +0200

    feat: use VITE_BASE_URL for router

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,7 +21,7 @@ async function tryScrollToAnchor(hash: string, timeout = 1000, delay = 100) {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: '/',
